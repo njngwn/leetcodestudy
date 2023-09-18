d660a12f2d42d1958ee89c352427229e968ff408
Jeongwon Na <jeongwon.na@tum.de>
2023-09-18 23:03:17 +0200

    Create fair-candy-swap.java

---
+++ study1/week5/fair-candy-swap.java
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/fair-candy-swap/
+// Time Complexity: O(n+m), n: aliceSizes.length, m: bobSizes.length
+// Space Complexity: O(1)
+class Solution {
+    public int[] fairCandySwap(int[] aliceSizes, int[] bobSizes) {
+        int aliceSum = 0;
+        int bobSum = 0;
+        for (int a : aliceSizes) { aliceSum += a; }
+        for (int b : bobSizes) { bobSum += b; }
+
+        int difference = (aliceSum - bobSum)/2;
+
+        for (int a = aliceSizes.length-1; a >= 0; --a) {
+            for (int b = 0; b < bobSizes.length; ++b) {
+                if (aliceSizes[a]-bobSizes[b] == difference) {
+                    return new int[]{aliceSizes[a], bobSizes[b]};
+                }
+            }
+        }
+
+        return new int[]{0, 0};
+    }
+}
